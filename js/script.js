bddcb98fab239aa98d384b9fb4b79f77e8b85074
Colin Hancock <colin1717@mac.com>
2016-04-08 12:09:17 -0500

    added functions for showRules and userTurn

--- js/script.js
+++ js/script.js
@@ -2,6 +2,11 @@ var board = [null, null, null,
              null, null, null,
              null, null, null];
 
+//change text in '.intro'
+function showRules(){
+  var introText = document.getElementById('introText');
+  introText.innerHTML = "Win Tic Tac Toe to keep this hotel from taking what's left of Jack's sanity. Don't loose 3 times.";
+}
 
 //determine if player or cpu goes first
 function coinToss(){
@@ -11,6 +16,12 @@ function coinToss(){
   } else {
     return 'cpu';
   }
+}
+
+//change text in startbutton to 'Your Turn'
+function userTurn(){
+  var buttonText = document.getElementById('startbuttontext');
+  buttonText.innerHTML = "Your Turn";
 }
 
 //cpu chooses a square
@@ -37,9 +48,33 @@ function cpuChoice(){
   } else {
     board[4] = 1;
   }
-};
+}
 
+//check for CPU win conditions
+function checkCpuWin(){
+  if (board[0] + board[1] + board[2] === 3){
+    console.log('CPU WIN!');
+  } else if (board[3] + board[4] + board[5] === 3){
+    console.log('CPU WIN!');
+  } else if (board[6] + board[7] + board[8] === 3){
+    console.log('CPU WIN!');
+  } else if (board[0] + board[3] + board[6] === 3){
+    console.log('CPU WIN!');
+  } else if (board[1] + board[4] + board[7] === 3){
+    console.log('CPU WIN!');
+  } else if (board[2] + board[5] + board[8] === 3){
+    console.log('CPU WIN!');
+  } else if (board[0] + board[4] + board[8] === 3){
+    console.log('CPU WIN!');
+  } else if (board[2] + board[4] + board[6] === 3){
+    console.log('CPU WIN!');
+  }
+}
 
-
+//Event Handlers
+//Startbutton
+$('#startbutton').click(function(){
+  showRules();
+})
 
 
